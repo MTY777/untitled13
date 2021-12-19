a73cd909b5bc0c50eb0fd44e9619dde31cc89d45
MTY777 <mataymekkurmanbek@gmail.com>
2021-12-19 19:02:32 +0600

    скажите где ошибка

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+Boss boss = new Boss();
+boss.getDefense("Магическая защита");
+boss.getDamage(50);
+boss.getHealt(700);
+        System.out.println("Атака Босса " + boss.getDamage(50)
+                + ", " + "HP Босса " + boss.getHealt(700) + ", " + boss.getDefense("Магическая Защита."));
+    }
+}
